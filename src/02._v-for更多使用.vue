b7278289ac6cf8e9c7187cfd62f253558f532867
jyefddz <1065697146@qq.com>
2022-06-30 18:02:04 +0800

    v-for更多使用

---
+++ src/02._v-for更多使用.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in stuArr" :key="item.id">{{ item.name }}-{{ item.sex }}-{{ item.hobby }}</li>
+        </ul>
+        <br>
+        <ul>
+            <li v-for="(item, key) in tObj" :key="item">{{ item }}-{{ key }}</li>
+        </ul>
+        <br>
+        <ul>
+            <li v-for="item in count" :key="item">{{ item }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            stuArr: [
+                {
+                    id: 1001,
+                    name: "孙悟空",
+                    sex: "男",
+                    hobby: "吃桃子",
+                },
+                {
+                    id: 1002,
+                    name: "猪八戒",
+                    sex: "男",
+                    hobby: "背媳妇",
+                },
+            ],
+            tObj: {
+                name: "小黑",
+                age: 18,
+                class: "1期",
+            },
+            count: 10
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <ul>
-      <li v-for="item in arr" :key="item">{{ item }}</li>
-      <li v-for="(item, index) in arr" :key="index">{{ item }}-{{ index }}</li>
+      <li v-for="item in stuArr" :key="item.id">{{ item.name }}-{{ item.sex }}-{{ item.hobby }}</li>
+    </ul>
+    <br>
+    <ul>
+      <li v-for="(item, key) in tObj" :key="item">{{ item }}-{{ key }}</li>
+    </ul>
+    <br>
+    <ul>
+      <li v-for="item in count" :key="item">{{ item }}</li>
     </ul>
   </div>
 </template>
@@ -11,7 +18,26 @@
 export default {
   data() {
     return {
-      arr: ['万叶', '钟离', '魈', '达达鸭']
+      stuArr: [
+        {
+          id: 1001,
+          name: "孙悟空",
+          sex: "男",
+          hobby: "吃桃子",
+        },
+        {
+          id: 1002,
+          name: "猪八戒",
+          sex: "男",
+          hobby: "背媳妇",
+        },
+      ],
+      tObj: {
+        name: "小黑",
+        age: 18,
+        class: "1期",
+      },
+      count: 10
     }
   }
 }
