d6c8eed3a6075e806ba3b466db2c349a113acd98
jyefddz <1065697146@qq.com>
2022-06-30 17:49:52 +0800

    v-for基本使用

---
+++ src/01._v-for基本使用.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in arr" :key="item">{{ item }}</li>
+            <li v-for="(item, index) in arr" :key="index">{{ item }}-{{ index }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            arr: ['万叶', '钟离', '魈', '达达鸭']
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ul>
+      <li v-for="item in arr" :key="item">{{ item }}</li>
+      <li v-for="(item, index) in arr" :key="index">{{ item }}-{{ index }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      arr: ['万叶', '钟离', '魈', '达达鸭']
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
