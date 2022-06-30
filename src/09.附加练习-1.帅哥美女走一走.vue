2d0921ea0990ad582d70f020797633a48ad030fc
jyefddz <1065697146@qq.com>
2022-06-30 20:36:09 +0800

    附加练习-1

---
+++ src/09.附加练习-1.帅哥美女走一走.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in myArr" :key="item">{{ item }}</li>
+        </ul>
+        <button @click="btn">走一走</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            myArr: ['万叶', '钟离', '魈']
+        }
+    },
+    methods: {
+        btn() {
+            this.myArr.push(this.myArr[0])
+            this.myArr.shift()
+        }
+    }
+}
+</script>
+
+<style>
+</style>
