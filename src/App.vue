af396d53d0557f3ed81ae7051d414eef81a9cee9
jyefddz <1065697146@qq.com>
2022-06-30 23:01:37 +0800

    附加练习-4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div id="app">
+  <div>
+    <p>请选择你要购买的书籍</p>
     <ul>
-      <li v-for="(item, ind) in arr" :key="item">
-        <span>{{ item }}</span>
-        <button @click="del(ind)">删除</button>
+      <li v-for="(item,id) in arr" :key="id">{{ item.name }}
+        <button @click="buyFn(id)">买书</button>
       </li>
     </ul>
-    <button @click="add">生成</button>
+    <table border="1" width="500" cellspacing="0">
+      <tr>
+        <th>序号</th>
+        <th>书名</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>合计</th>
+      </tr>
+      <tr v-for="(item, index) in arr" :key="index">
+        <td>{{ index + 1 }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.price }}</td>
+        <td>{{ item.count }}</td>
+        <td>{{ (item.count) * (item.price) }}</td>
+      </tr>
+    </table>
+    <p>总价格为: {{ sumPrice }}</p>
   </div>
 </template>
 
@@ -14,15 +30,41 @@
 export default {
   data() {
     return {
-      arr: [1, 5, 3],
+      arr: [
+        {
+          name: "水浒传",
+          price: 107,
+          count: 0,
+        },
+        {
+          name: "西游记",
+          price: 192,
+          count: 0,
+        },
+        {
+          name: "三国演义",
+          price: 219,
+          count: 0,
+        },
+        {
+          name: "红楼梦",
+          price: 178,
+          count: 0,
+        },
+      ],
     };
   },
   methods: {
-    add() {
-      this.arr.push(Math.floor(Math.random() * 20));
-    },
-    del(index) {
-      this.arr.splice(index, 1);
+    buyFn(index) {
+      this.arr[index].count++
+    }
+  },
+  computed: {
+    sumPrice() {
+      // 数组里放的是对象, 而对象是复杂类型, 引用关系, 值改变会触发计算属性重新执行
+      return this.arr.reduce((sum, obj) => {
+        return (sum += obj["price"] * obj["count"]);
+      }, 0);
     },
   },
 };
