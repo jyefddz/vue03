4b12e5be0c3eb9625badb667821ddd6faff6476a
jyefddz <1065697146@qq.com>
2022-06-30 19:51:53 +0800

    动态class

---
+++ src/05._动态class.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p :class="{ active: activeFlag }" class="ppp">我是摩拉克斯,你有带钱吗</p>
+        <button @click="activeFlag = !activeFlag">click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeFlag: false
+        }
+    }
+}
+</script>
+
+<style>
+.active {
+    color: rgb(0, 225, 255);
+}
+
+.ppp {
+    border: 10px solid rgb(190, 244, 159);
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h1>{{ obj.a }}</h1>
-    <button @click="change">改变</button>
-    <br>
-    <h1>{{ obj1 && obj1.a }}</h1>
-    <button @click="change1">改变1</button>
+    <p :class="{ active: activeFlag }" class="ppp">我是摩拉克斯,你有带钱吗</p>
+    <button @click="activeFlag = !activeFlag">click me</button>
   </div>
 </template>
 
@@ -12,26 +9,18 @@
 export default {
   data() {
     return {
-      obj: {
-        b: 11,
-        c: 12
-      },
-      obj1: {}
-    }
-  },
-  methods: {
-    change() {
-      this.$set(this.obj, 'a', 9)
-      console.log(this.obj);
-    },
-    change1() {
-      this.obj1 = {
-        a: 20
-      }
+      activeFlag: false
     }
   }
 }
 </script>
 
 <style>
+.active {
+  color: rgb(0, 225, 255);
+}
+
+.ppp {
+  border: 10px solid rgb(190, 244, 159);
+}
 </style>
